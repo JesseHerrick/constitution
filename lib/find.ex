cf08e651d3cf50b6e6e9f11e59c835d482955199
Jesse Herrick <me@jesseherrick.com>
2018-02-13 16:55:46 -0500

    Add more parts of the Constitution and revise the Find module code

--- lib/find.ex
+++ lib/find.ex
@@ -1,28 +1,34 @@
 defmodule Constitution.Find do
   @original_path "data/original/"
-  @amendment_path "data/original/"
+  @amendment_path "data/amendment/"
 
-  def article(number) do
-    "ARTICLE #{number}\n\n" <> File.read!(article_path(number))
-    |> clean_tags
-  end
-
-  def clean_tags(text) do
-    Regex.replace(~r/{{ .+ }}/, text, "", global: true)
-    |> String.replace("\n\n\n\n", "\n\n", global: true)
-    |> String.replace("\nSECTION", "SECTION", global: true)
-  end
-
-  def section(number) do
+  def amendment_path(number) do
+    @amendment_path <> "amendment-#{number}.txt"
   end
 
   def article_path(number) do
     @original_path <> "article-#{String.downcase(number)}.txt"
   end
 
-  def find_part(file, tag_name) do
+  def clean_tags(nil), do: nil
+  def clean_tags(text) do
+    Regex.replace(~r/{{ .+ }}/, text, "", global: true)
+    |> String.replace("\n\n\n\n", "\n\n", global: true)
+    |> String.replace("\nSECTION", "SECTION", global: true)
+  end
+
+  def part(file, tag_name) do
     ~r/{{ BEGIN #{tag_name} }}\n(.+)\n{{ END #{tag_name} }}/s
     |> Regex.run(File.read!(file))
+    |> clean_part
+  end
+
+  def clean_part(nil) do
+    ""
+  end
+
+  def clean_part(selection) do
+    selection
     |> Enum.at(1)
     |> clean_tags
   end
